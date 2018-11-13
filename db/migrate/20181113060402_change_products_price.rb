e533782ee1ebfa6d1d4e8f045bcc71ac1223fd55
chionyan <ariariarissa@gmail.com>
2018-11-13 15:06:13 +0900

    $ rails g migration ChangeProductsPrice

---
+++ db/migrate/20181113060402_change_products_price.rb
@@ -0,0 +1,13 @@
+class ChangeProductsPrice < ActiveRecord::Migration[5.2]
+  def up
+    change_table :products do |t|
+      t.change :price, :string
+    end
+  end
+
+  def down
+    change_table :products do |t|
+      t.change :price, 'integer USING CAST(price AS integer)'
+    end
+  end
+end
