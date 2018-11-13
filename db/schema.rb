3026bec0c30f2a7e00a612032ada062e6de312d7
chionyan <ariariarissa@gmail.com>
2018-11-13 18:00:45 +0900

    $ rails g migration AddProductNumberToProducts product_number:string:index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_061512) do
+ActiveRecord::Schema.define(version: 2018_11_13_085132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2018_11_13_061512) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "part_number"
+    t.string "product_number"
+    t.index ["product_number"], name: "index_products_on_product_number"
   end
 
   create_table "users", force: :cascade do |t|
