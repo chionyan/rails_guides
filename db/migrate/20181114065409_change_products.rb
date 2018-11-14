9e2b3ece645c214753f13e055cc7b5eea09f3764
chionyan <ariariarissa@gmail.com>
2018-11-14 15:54:59 +0900

    3.3 テーブルを変更する

---
+++ db/migrate/20181114065409_change_products.rb
@@ -0,0 +1,10 @@
+class ChangeProducts < ActiveRecord::Migration[5.2]
+  def change
+    change_table :products do |t|
+      t.remove :description, :name
+      t.string :part_number
+      t.index :part_number
+      t.rename :upccode, :upc_code
+    end
+  end
+end
