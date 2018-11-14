5a3d633277e01059d4940c7810b5e7cdb55fda51
chionyan <ariariarissa@gmail.com>
2018-11-14 17:20:07 +0900

    $ rails g migration AddAuthorIdToArticles author_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_081104) do
+ActiveRecord::Schema.define(version: 2018_11_14_081845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_081104) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
   create_table "authors", force: :cascade do |t|
