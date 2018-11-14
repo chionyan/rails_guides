77a0e53fd74f0980ceb13116e9cad0fdf41e48a2
chionyan <ariariarissa@gmail.com>
2018-11-14 17:11:52 +0900

    $ rails g migration CreateAuthors name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_071739) do
+ActiveRecord::Schema.define(version: 2018_11_14_081104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2018_11_14_071739) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "categories_products", id: false, force: :cascade do |t|
