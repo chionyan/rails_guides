3c809c215369c3370bd58908c2436529b1de55b8
chionyan <ariariarissa@gmail.com>
2018-11-14 18:29:54 +0900

    3.10 up/downメソッドを使用する

--- db/migrate/20181114085454_example_migration.rb
+++ db/migrate/20181114085454_example_migration.rb
@@ -1,27 +1,28 @@
 class ExampleMigration < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :distributors do |t|
       t.string :zipcode
     end
 
-    reversible do |dir|
-      dir.up do
-        execute <<-SQL
-          ALTER TABLE distributors
-            ADD CONSTRAINT zipchk
-              CHECK (char_length(zipcode) = 5) NO INHERIT;
-        SQL
-        add_column :users, :home_page_url, :string
-        rename_column :users, :email, :email_address
-      end
-      dir.down do
-        execute <<-SQL
-          ALTER TABLE distributors
-            DROP CONSTRAINT zipchk
-        SQL
-        remove_column :users, :home_page_url
-        rename_column :users, :email_address, :email
-      end
-    end
+    execute <<-SQL
+    ALTER TABLE distributors
+      ADD CONSTRAINT zipchk
+        CHECK (char_length(zipcode) = 5) NO INHERIT;
+    SQL
+
+    add_column :users, :home_page_url, :string
+    rename_column :users, :email, :email_address
+  end
+
+  def down
+    rename_column :users, :email_address, :email
+    remove_column :users, :home_page_url
+
+    execute <<-SQL
+    ALTER TABLE distributors
+      DROP CONSTRAINT zipchk
+    SQL
+
+    drop_table :distributors
   end
 end
