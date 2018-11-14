43ff502b4b5a36db1b02942e63fff69d1057d022
chionyan <ariariarissa@gmail.com>
2018-11-14 17:54:25 +0900

    $ rails g migration AddEmailToUser email:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_082328) do
+ActiveRecord::Schema.define(version: 2018_11_14_085347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_082328) do
     t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   add_foreign_key "comments", "articles"
