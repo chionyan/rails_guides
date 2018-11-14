942c26ab709d6dfa48f994217e94d9606c4d741a
chionyan <ariariarissa@gmail.com>
2018-11-14 12:33:24 +0900

    3.1 テーブルを作成する

--- db/migrate/20181114032207_create_products.rb
+++ db/migrate/20181114032207_create_products.rb
@@ -1,10 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
-    create_table :products do |t|
+    create_table :products, comment: 'プロダクトのテーブル' do |t|
       t.string :name
-      t.text :description
-
-      t.timestamps
     end
   end
 end
