5abf1205d45180bc3678073fdf8490e85b243ffc
chionyan <ariariarissa@gmail.com>
2018-11-15 17:29:57 +0900

    db: Add legacy_code To users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_081316) do
+ActiveRecord::Schema.define(version: 2018_11_15_082857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_081316) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "subdomain"
+    t.string "legacy_code"
   end
 
   add_foreign_key "articles", "authors"
