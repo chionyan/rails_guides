cfced9a5e902766a250cf4fa9083b4f0aff1cd0f
chionyan <ariariarissa@gmail.com>
2018-11-15 17:43:10 +0900

    2.6 inclusion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :email, confirmation: true
   validates :subdomain, exclusion: { in: %w(www us ca jp), message: "%{value}は予約済みです" }
   validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: "英文字のみが使用できます" }
+  validates :size, inclusion: { in: %w(small medium large), message: "%{value} のサイズは無効です" }
 end
