a50e2c5a9e4d4c595ae594e3c62048152e378fc6
chionyan <ariariarissa@gmail.com>
2018-11-15 17:38:31 +0900

    db: Add size To users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_082857) do
+ActiveRecord::Schema.define(version: 2018_11_15_083745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_082857) do
     t.string "email"
     t.string "subdomain"
     t.string "legacy_code"
+    t.string "size"
   end
 
   add_foreign_key "articles", "authors"
