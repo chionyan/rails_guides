de835499377f3e7b4a0022cb08f90f31ba138f23
chionyan <ariariarissa@gmail.com>
2018-11-15 17:24:42 +0900

    2.4 exclusion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, confirmation: true
+  validates :subdomain, exclusion: { in: %w(www us ca jp), message: "%{value}は予約済みです" }
 end
