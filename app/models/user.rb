0f83fd100bfbcba3bfbcd58e38bae8f2b2488b9e
chionyan <ariariarissa@gmail.com>
2018-11-15 17:33:05 +0900

    2.5 format

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, confirmation: true
   validates :subdomain, exclusion: { in: %w(www us ca jp), message: "%{value}は予約済みです" }
+  validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: "英文字のみが使用できます" }
 end
