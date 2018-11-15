81c4b7fb2183250018b91085ad202682a73631fc
chionyan <ariariarissa@gmail.com>
2018-11-15 18:08:02 +0900

    2.7 length(maximum)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :subdomain, exclusion: { in: %w(www us ca jp), message: "%{value}は予約済みです" }
   validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: "英文字のみが使用できます" }
   validates :size, inclusion: { in: %w(small medium large), message: "%{value} のサイズは無効です" }
+  validates :bio, length: { maximum: 500 }
 end
