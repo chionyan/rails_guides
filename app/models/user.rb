c7bafb48a68c6131d6eb5c2e56161ef68c807025
chionyan <ariariarissa@gmail.com>
2018-11-16 11:16:25 +0900

    2.8 numericality

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :bio, length: { maximum: 500 }
   validates :password, length: { in: 6..20 }
   validates :registration_number, length: { is: 6 }
+  validates :points, numericality: true
 end
