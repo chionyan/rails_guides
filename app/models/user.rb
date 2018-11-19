fe8676e8a46ce622b1c7687dc17e9912ca766890
chionyan <ariariarissa@gmail.com>
2018-11-19 18:46:15 +0900

    5.1 :ifや:unlessでシンボルを使用する

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,9 @@ class User < ApplicationRecord
                 Try again #{Time.zone.tomorrow}"
               end,
             }
+  validates :card_number, presence: true, if: :paid_with_card?
+
+  def paid_with_card?
+    payment_type == 'card'
+  end
 end
