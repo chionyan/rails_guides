5f8dc742469898f298b854ba1eda4044c149be21
chionyan <ariariarissa@gmail.com>
2018-11-19 16:07:15 +0900

    db: Create Holiday Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_053023) do
+ActiveRecord::Schema.define(version: 2018_11_19_070619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2018_11_19_053023) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "holidays", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.integer "month"
+    t.integer "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "line_items", force: :cascade do |t|
