3bb869d206714697e479d27accd83dd13d716ee2
chionyan <ariariarissa@gmail.com>
2018-11-19 15:57:15 +0900

    db: Add User Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'hoge',
-            email: 'hoge@example.com',
-            legacy_code: 'abc',
-            size: 'large',
-            bio: 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDE',
-            password: 'ABCDEF',
-            registration_number: '123456',
-            points: 1.25,
-            games_played: 6,)
+2.times do |i|
+  User.create(name: "hoge#{i}",
+              email: "hoge#{i}@example.com",
+              legacy_code: 'abc',
+              size: 'large',
+              bio: 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDE',
+              password: 'ABCDEF',
+              registration_number: '123456',
+              points: 1.25,
+              games_played: 6,
+              boolean_field: false,)
+end
 
 5.times do |i|
   Product.create(name: "Product ##{i}", description: 'A product.')
