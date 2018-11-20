f6260b21f6ebc7be5c77b9710c292b25810c733c
chionyan <ariariarissa@gmail.com>
2018-11-20 14:35:40 +0900

    5.3 条件付きバリデーションをグループ化する

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,10 @@ end
 
 class User < ApplicationRecord
   validates :name, presence: { strict: true }
-  validates :email, confirmation: true, uniqueness: true, on: :update
   validates :subdomain, exclusion: { in: %w[www us ca jp], message: '%{value}は予約済みです' }
   validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: '英文字のみが使用できます' }
   validates :size, inclusion: { in: %w[small medium large], message: '%{value} のサイズは無効です' }, allow_blank: true
   validates :bio, length: { maximum: 500 }
-  validates :password, confirmation: true,
-                       unless: Proc.new { |a| a.password.blank? }
   validates :registration_number, length: { is: 6 }
   validates :points, numericality: true
   validates :games_played, numericality: { even: true }
@@ -34,6 +31,15 @@ class User < ApplicationRecord
               end,
             }
   validates :card_number, presence: true, if: :paid_with_card?
+
+  with_options if: :admin? do
+    validates :password, length: { minimum: 10 }
+    validates :email, presence: true
+  end
+
+  def admin?
+    name == 'admin'
+  end
 
   def paid_with_card?
     payment_type == 'card'
