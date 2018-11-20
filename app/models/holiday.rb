aec890ff7bf1cd2faa6f22ffa7f5acb364fdc41e
chionyan <ariariarissa@gmail.com>
2018-11-20 19:16:54 +0900

    modify: Delete Unnecessary Models

--- app/models/holiday.rb
+++
@@ -1,4 +0,0 @@
-class Holiday < ApplicationRecord
-  validates :name, uniqueness: { scope: :year,
-                                 message: '発生は年に1度までである必要があります', }
-end
--- app/models/line_item.rb
+++
@@ -1,4 +0,0 @@
-class LineItem < ApplicationRecord
-  belongs_to :order
-  validates :order, absence: true
-end
