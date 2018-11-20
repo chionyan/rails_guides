236d5d0bdea90e7dd1f4aa51ca2a73ca0701a1d1
chionyan <ariariarissa@gmail.com>
2018-11-20 14:55:46 +0900

    5.4 バリデーションの条件を結合する

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,2 +1,19 @@
 class Computer < ApplicationRecord
+  validates :mouse, presence: true,
+                    if: [Proc.new { |c| c.market.retail? }, :desktop?],
+                    unless: Proc.new { |c| c.trackpad.present? }
+
+  class Market
+    def retail?
+      true
+    end
+  end
+
+  def market
+    Market.new
+  end
+
+  def desktop?
+    true
+  end
 end
