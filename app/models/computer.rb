36bdf9be86ac567aa4560a63e6c2e7e3df389855
chionyan <ariariarissa@gmail.com>
2018-11-20 17:09:38 +0900

    refactor: Organizing Codes

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,7 +1,7 @@
 class Computer < ApplicationRecord
   validates :mouse, presence: true,
-                    if: [Proc.new { |c| c.market.retail? }, :desktop?],
-                    unless: Proc.new { |c| c.trackpad.present? }
+                    if: [proc { |c| c.market.retail? }, :desktop?],
+                    unless: proc { |c| c.trackpad.present? }
 
   class Market
     def retail?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,12 @@
-class GoodnessValidator < ActiveModel::Validator
-  def validate(record)
-    record.errors[:base] << 'これは悪人だ' if options[:fields].any? { |field| record.send(field) == 'Evil' }
-  end
-end
-
 class MyValidator < ActiveModel::Validator
   def validate(record)
-    unless record.name.starts_with? 'X'
-      record.errors[:name] << '名前はXで始まる必要があります'
-    end
+    record.errors[:name] << '名前はhで始まる必要があります' unless record.name.starts_with? 'h'
   end
 end
 
 class User < ApplicationRecord
   validates :name, presence: { strict: true }
-  validates :subdomain, exclusion: { in: %w[www us ca jp], message: '%{value}は予約済みです' }
+  validates :email, presence: true
   validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: '英文字のみが使用できます' }
   validates :size, inclusion: { in: %w[small medium large], message: '%{value} のサイズは無効です' }, allow_blank: true
   validates :bio, length: { maximum: 500 }
@@ -23,34 +15,6 @@ class User < ApplicationRecord
   validates :games_played, numericality: { even: true }
   validates :boolean_field, inclusion: { in: [true, false] }
   validates :boolean_field, exclusion: { in: [nil] }
-  validates :occupation, absence: true
 
   validates_with MyValidator
-  validates_with GoodnessValidator, fields: [:first_name, :last_name]
-  validates_each :first_name, :last_name do |record, attr, value|
-    record.errors.add(attr, 'must start with upper case') if value =~ /\A[a-z]/
-  end
-
-  validates :age, numericality: { message: '%{value} seems wrong' }
-  validates :username,
-            uniqueness: {
-              message: lambda do |object, data|
-                "Hey #{object.name}!, #{data[:value]} is taken already!
-                Try again #{Time.zone.tomorrow}"
-              end,
-            }
-  validates :card_number, presence: true, if: :paid_with_card?
-
-  with_options if: :admin? do
-    validates :password, length: { minimum: 10 }
-    validates :email, presence: true
-  end
-
-  def admin?
-    name == 'admin'
-  end
-
-  def paid_with_card?
-    payment_type == 'card'
-  end
 end
