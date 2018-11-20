9a9ef16e5ec7fb6f16ead53766d77b12a6961f28
chionyan <ariariarissa@gmail.com>
2018-11-20 17:37:37 +0900

    6.2 カスタムメソッド 1

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,7 @@
 class Invoice < ApplicationRecord
+  validate :expiration_date_cannot_be_in_the_past
+
+  def expiration_date_cannot_be_in_the_past
+    errors.add(:expiration_date, '：過去の日付は使用できません') if expiration_date.present? && expiration_date < Time.zone.today
+  end
 end
