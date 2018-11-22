5ca0a0ab79346e380d4b04ce5ecbef19ee0e2f94
chionyan <ariariarissa@gmail.com>
2018-11-22 16:32:04 +0900

    feat: Callback list(Except around_xxx)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,33 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/.match?(value)
-      record.errors[attribute] << (options[:message] || 'はメールアドレスではありません')
-    end
-  end
-end
-
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true, email: true
-  validates :password, length: { in: 6..20 }
-  validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: '英文字のみが使用できます' }
-  validates :size, inclusion: { in: %w[small medium large], message: '%{value} のサイズは無効です' }, allow_blank: true
-  validates :points, numericality: true
-  validates :boolean_field, inclusion: { in: [true, false] }
-  validates :boolean_field, exclusion: { in: [nil] }
 
-  after_initialize do |_user|
-    puts 'オブジェクトは初期化されました'
-  end
+  before_validation { puts 'Called "before_validation"' }
+  after_validation { puts 'Called "after_validation"' }
 
-  after_find do |_user|
-    puts 'オブジェクトが見つかりました'
-  end
+  before_save { puts 'Called "before_save"' }
+  after_save { puts 'Called "after_save"' }
 
-  after_touch do |_user|
-    puts 'オブジェクトにタッチしました'
-  end
+  before_create { puts 'Called "before_create"' }
+  after_create { puts 'Called "after_create"' }
+
+  before_update { puts 'Called "before_update"' }
+  after_update { puts 'Called "after_update"' }
+
+  before_destroy { puts 'Called "before_destroy"' }
+  after_destroy { puts 'Called "after_destroy"' }
+
+  after_initialize { puts 'Called "after_initialize"' }
+  after_find { puts 'Called "after_find"' }
+  after_touch { puts 'Called "after_touch"' }
+
+  after_commit { puts 'Called "after_commit"' }
+  after_rollback { puts 'Called "after_rollback"' }
+  after_create_commit { puts 'Called "after_create_commit"' }
+  after_update_commit { puts 'Called "after_update_commit"' }
+  after_destroy_commit { puts 'Called "after_destroy_commit"' }
+
+  # around_save { puts 'Called "around_save"' }
+  # around_create { puts 'Called "around_create"' }
+  # around_update { puts 'Called "around_update"' }
+  # around_destroy { puts 'Called "around_destroy"' }
 end
