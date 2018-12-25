d3a909dc05f34b0158fe8e65deabb9a44e949bfa
chionyan <ariariarissa@gmail.com>
2018-12-25 12:21:34 +0900

    9 コールバッククラス

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,20 @@
+class ProductCallbacks
+  def self.before_destroy(product)
+    if product.product_number.blank?
+      Product.find_by(name: product.product_number).delete
+    else
+      throw :abort
+    end
+  end
+end
+
 class Product < ApplicationRecord
   belongs_to :user, counter_cache: :products_count
 
   before_save :normalize_product_number, if: :name?,
     unless: Proc.new { self.name == 'hoge' }
+
+  before_destroy ProductCallbacks
 
   private
 
