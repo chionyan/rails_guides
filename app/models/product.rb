5707ca169c4cdb2539470f8348cdc800775276d5
chionyan <ariariarissa@gmail.com>
2018-12-25 11:10:52 +0900

    7 リレーションシップのコールバック

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user, counter_cache: :products_count
+
+  after_destroy :log_destroy_action
+
+  def log_destroy_action
+    puts 'Post destroyed'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :products
+  has_many :products, dependent: :destroy
 
   validates :name, presence: true
 
