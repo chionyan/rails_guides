c7b84a5c225f8d7011ab4f2aab2465083167ce4e
chionyan <ariariarissa@gmail.com>
2018-12-25 11:49:34 +0900

    8.3 コールバックで複数の条件を指定する

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :user, counter_cache: :products_count
 
-  before_save :normalize_product_number,
-    if: Proc.new { self.name.present? }
+  before_save :normalize_product_number, if: :name?,
+    unless: Proc.new { self.name == 'hoge' }
 
   private
 
   def normalize_product_number
     self.product_number = '123456'
+  end
+
+  def name?
+    self.name.present?
   end
 end
